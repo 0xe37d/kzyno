6386d5392bf0a6194571462b6dfe89481de030b6
0xe37 <0xe37@proton.me>
2025-02-04 23:10:01 -0500

    chore: update commit hash

--- frontend/src/app/commit-hash.ts
+++ frontend/src/app/commit-hash.ts
@@ -1,1 +1,1 @@
-export const NEXT_PUBLIC_COMMIT_HASH: string = '917700cdbed5969d801c4a309dc97a2611fe4ddb';
+export const NEXT_PUBLIC_COMMIT_HASH: string = 'e7c38c8512156ca82e04616542d8dda73225df7b';
