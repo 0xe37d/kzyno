2147927db695196300238a3b69c8d54f65efad1f
0xe37d <0xe37d@kzyno.com>
2025-03-16 13:38:09 -0500

    chore: update commit hash

--- frontend/src/app/commit-hash.ts
+++ frontend/src/app/commit-hash.ts
@@ -1,1 +1,1 @@
-export const NEXT_PUBLIC_COMMIT_HASH: string = '25aa8dc570a3814a87f3049a1236accba0a10a59';
+export const NEXT_PUBLIC_COMMIT_HASH: string = 'b66177ea94f040ed0417d68e65d2c4fd3dad918a';
